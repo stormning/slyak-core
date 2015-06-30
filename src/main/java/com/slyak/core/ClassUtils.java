895b8e9127880e23c9ab8a3413a24d5b9ea3cbc7
stormning <stormning@163.com>
2015-06-30 20:06:07 +0800

    classutils

--- src/main/java/com/slyak/core/ClassUtils.java
+++ src/main/java/com/slyak/core/ClassUtils.java
@@ -1,8 +1,11 @@
 package com.slyak.core;
 
+import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * .
@@ -12,6 +15,8 @@ import java.lang.reflect.Type;
  * @version V1.0, 2015/6/30
  */
 public class ClassUtils {
+
+    private ClassUtils(){}
 
     public static <T> Class<T> getGenericParameter(Class clazz, int index) {
         Type genType = clazz.getGenericSuperclass();
@@ -43,5 +48,70 @@ public class ClassUtils {
             return (Class) param;
         }
         return null;
+    }
+
+    public static Set<Field> getAnnotatedFields(Class clazz,Class<? extends Annotation>... annotations) {
+        return doInAnnotatedFields(clazz,new SetFieldExtractor() ,annotations);
+    }
+
+    public static Field getAnnotatedField(Class clazz,Class<? extends Annotation>... annotations) {
+        return doInAnnotatedFields(clazz,new SingleFieldExtractor() ,annotations);
+    }
+
+    public static <T> T doInAnnotatedFields(Class clazz,FieldExtractor<T> extractor, Class<? extends Annotation>... annotations) {
+        for (Field field : clazz.getDeclaredFields()) {
+            int hit = 0;
+            for (Class<? extends Annotation> annotation : annotations) {
+                if (field.isAnnotationPresent(annotation)){
+                    hit++;
+                } else {
+                    break;
+                }
+            }
+            if (hit == annotations.length) {
+                if (!extractor.extractNext(field)){
+                    return extractor.getReturn();
+                }
+            }
+        }
+        Class parent = clazz.getSuperclass();
+        if (parent != null) {
+            return doInAnnotatedFields(parent,extractor,annotations);
+        }
+        return extractor.getReturn();
+    }
+
+    private static class SetFieldExtractor implements FieldExtractor<Set<Field>> {
+        Set<Field> result = new HashSet<Field>();
+
+        @Override
+        public boolean extractNext(Field field) {
+            result.add(field);
+            return true;
+        }
+
+        @Override
+        public Set<Field> getReturn() {
+            return result;
+        }
+    }
+
+    private static class SingleFieldExtractor implements FieldExtractor<Field> {
+        Field field = null;
+        @Override
+        public boolean extractNext(Field field) {
+            this.field = field;
+            return false;
+        }
+
+        @Override
+        public Field getReturn() {
+            return field;
+        }
+    }
+
+    public static interface FieldExtractor<T> {
+        boolean extractNext(Field field);
+        T getReturn();
     }
 }
