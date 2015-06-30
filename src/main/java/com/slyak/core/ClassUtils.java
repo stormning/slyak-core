6f87fb6724de0c96d87039c0600a1f89d30e82a1
stormning <stormning@163.com>
2015-06-30 20:12:16 +0800

    classutils

--- src/main/java/com/slyak/core/ClassUtils.java
+++ src/main/java/com/slyak/core/ClassUtils.java
@@ -1,11 +1,8 @@
 package com.slyak.core;
 
-import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
-import java.util.HashSet;
-import java.util.Set;
 
 /**
  * .
@@ -16,102 +13,38 @@ import java.util.Set;
  */
 public class ClassUtils {
 
-    private ClassUtils(){}
+    private ClassUtils() {
+    }
 
-    public static <T> Class<T> getGenericParameter(Class clazz, int index) {
+    public static Class getGenericParameter(Class clazz, int index) {
         Type genType = clazz.getGenericSuperclass();
         if (genType instanceof ParameterizedType) {
-            return getGenericParameter((ParameterizedType)genType, index);
+            return getGenericParameter((ParameterizedType) genType, index);
         }
         return null;
     }
 
-    public static <T> Class<T> getGenericParameter0(Class clazz) {
+    public static Class getGenericParameter0(Class clazz) {
         return getGenericParameter(clazz, 0);
     }
 
-    public static <T> Class<T> getGenericParameter(Field field, int index) {
+    public static Class getGenericParameter(Field field, int index) {
         Type genType = field.getGenericType();
         if (genType instanceof ParameterizedType) {
-            return getGenericParameter((ParameterizedType)genType, index);
+            return getGenericParameter((ParameterizedType) genType, index);
         }
         return null;
     }
 
-    public static <T> Class<T> getGenericParameter0(Field field) {
+    public static Class getGenericParameter0(Field field) {
         return getGenericParameter(field, 0);
     }
 
-    private static <T> Class<T> getGenericParameter(ParameterizedType type, int index) {
+    private static Class getGenericParameter(ParameterizedType type, int index) {
         Type param = type.getActualTypeArguments()[index];
         if (param instanceof Class) {
             return (Class) param;
         }
         return null;
-    }
-
-    public static Set<Field> getAnnotatedFields(Class clazz,Class<? extends Annotation>... annotations) {
-        return doInAnnotatedFields(clazz,new SetFieldExtractor() ,annotations);
-    }
-
-    public static Field getAnnotatedField(Class clazz,Class<? extends Annotation>... annotations) {
-        return doInAnnotatedFields(clazz,new SingleFieldExtractor() ,annotations);
-    }
-
-    public static <T> T doInAnnotatedFields(Class clazz,FieldExtractor<T> extractor, Class<? extends Annotation>... annotations) {
-        for (Field field : clazz.getDeclaredFields()) {
-            int hit = 0;
-            for (Class<? extends Annotation> annotation : annotations) {
-                if (field.isAnnotationPresent(annotation)){
-                    hit++;
-                } else {
-                    break;
-                }
-            }
-            if (hit == annotations.length) {
-                if (!extractor.extractNext(field)){
-                    return extractor.getReturn();
-                }
-            }
-        }
-        Class parent = clazz.getSuperclass();
-        if (parent != null) {
-            return doInAnnotatedFields(parent,extractor,annotations);
-        }
-        return extractor.getReturn();
-    }
-
-    private static class SetFieldExtractor implements FieldExtractor<Set<Field>> {
-        Set<Field> result = new HashSet<Field>();
-
-        @Override
-        public boolean extractNext(Field field) {
-            result.add(field);
-            return true;
-        }
-
-        @Override
-        public Set<Field> getReturn() {
-            return result;
-        }
-    }
-
-    private static class SingleFieldExtractor implements FieldExtractor<Field> {
-        Field field = null;
-        @Override
-        public boolean extractNext(Field field) {
-            this.field = field;
-            return false;
-        }
-
-        @Override
-        public Field getReturn() {
-            return field;
-        }
-    }
-
-    public static interface FieldExtractor<T> {
-        boolean extractNext(Field field);
-        T getReturn();
     }
 }
